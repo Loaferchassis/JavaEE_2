dec6d9d1b7970673e2005c04adc3cbbc733058a0
loaferchassis <loaferchassis@gmail.com>
2018-04-07 19:00:37 +0300

    They are back!

---
+++ src/main/java/com/ser/gey/Application.java
@@ -0,0 +1,14 @@
+package com.ser.gey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
